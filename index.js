e13af9c10375bda6840d0b53946d24c657db9f2e
Oyetunji Ibukunoluwa Oluwole <78256143+Ibukun-tech@users.noreply.github.com>
2024-08-26 16:21:52 +0100

    [feature]:finish implementing the airtime feature

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import axios from "axios";
 import Wallet from "./lib/Wallets/wallet";
 import Self from "./lib/Self/self";
 import Card from "./lib/Card/card";
+import airtime from "./lib/Airtime"
+import Airtime from "./lib/Airtime/airtime";
 /** 
  * @class VirtualWallet
  */
@@ -10,6 +12,7 @@ class VirtualWallet{
      static wallet=Wallet
      static self= Self
      static card=Card
+     static airtime= Airtime
      // public Bank
     /**
      * 
@@ -24,6 +27,7 @@ class VirtualWallet{
         this.wallet.secretKey=options.secretKey
         this.self.secretKey=options.secretKey
         this.card.secretKey=options.secretKey
+        this.airtime.secretKey=options.secretKey
     }
 }
 export default VirtualWallet
